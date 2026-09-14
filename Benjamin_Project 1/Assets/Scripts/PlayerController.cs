13c4a6bc8b3471b433b8e027392f1827590dfd37
Oceanblade2018-Kolt <koltynbenjamin@gmail.com>
2026-09-14 09:57:30 -0700

    Implemented Jump and Movement

--- Benjamin_Project 1/Assets/Scripts/PlayerController.cs
+++ Benjamin_Project 1/Assets/Scripts/PlayerController.cs
@@ -5,9 +5,11 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
     public float jumpHeight = 10f;
+    public float jumpDetectionDistance = 1.1f;
 
     PlayerInput PlayerInput;
     Rigidbody rb;
+    Ray jumpRay;
 
     Vector2 moveInput;
 
@@ -21,14 +23,22 @@ public class PlayerController : MonoBehaviour
 
         //Setting up new move Vector
         moveInput = new Vector2();
+        jumpRay = new Ray(transform.position, -transform.up);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 tempMovement = rb.linearVelocity;
-        rb.linearVelocity = (moveInput.x * speed) + (moveInput.y * speed) + (moveInput.z * speed);
+        jumpRay.origin = transform.position;
+        jumpRay.direction = -transform.up;
 
+        Vector3 tempMove = rb.linearVelocity;
+
+        //rb.linearVelocity = (moveInput.x * speed) + (moveInput.y * speed) + (moveInput.z * speed);
+        tempMove.x = (moveInput.x * speed) * transform.right.x;
+        tempMove.z = (moveInput.y * speed) * transform.forward.z;
+
+        rb.linearVelocity = tempMove;
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -37,6 +47,14 @@ public class PlayerController : MonoBehaviour
         moveInput = context.ReadValue<Vector2>();
     }
 
+    public void Jump()
+    {
+        if(Physics.Raycast(jumpRay, jumpDetectionDistance))
+        {
+            rb.AddForce(transform.up * jumpHeight, ForceMode.Impulse);
+        }
 
+        //rb.AddForce(Vector3.up * jumpHeight);
+    }
 
 }
